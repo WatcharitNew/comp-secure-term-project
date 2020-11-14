e1ac07a343a02e91f697b42b95464191404a23ad
Sumet Chorthongdee <sumetchor@gmail.com>
2020-11-14 21:22:23 +0700

    show post and comment / update style

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,35 +1,59 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { useHistory } from "react-router";
-import axios from 'axios';
+import axios from "axios";
 
 const ENDPOINT = process.env.REACT_APP_BACKEND;
 
 const HomeComponent = () => {
   const [posts, setPosts] = useState([]);
 
-  axios.defaults.headers.common["Authorization"] = "Bearer " + sessionStorage.getItem("access_token");
-  useEffect(() => {
-    axios.get(`${ENDPOINT}/posts`).then((res) => {
-      console.log(res.status);
-      if(res.status===200) {
-        setPosts(res.data);
+  axios.defaults.headers.common["Authorization"] =
+    "Bearer " + sessionStorage.getItem("access_token");
+  useEffect(async () => {
+    let posts, comments;
+    const posts_res = await axios.get(`${ENDPOINT}/posts`);
+    if (posts_res.status === 200) {
+      posts = posts_res.data;
+    } else {
+      console.log(`Error: ${ENDPOINT}/posts return status ${posts_res.status}`);
+      return;
+    }
+    const comments_res = await axios.get(`${ENDPOINT}/comments`);
+    if (comments_res.status === 200) {
+      comments = comments_res.data;
+    } else {
+      console.log(
+        `Error: ${ENDPOINT}/posts return status ${comments_res.status}`
+      );
+      return;
+    }
+    let postIdToIdx = {};
+    posts.forEach((post, idx) => {
+      const { _id } = post;
+      postIdToIdx[_id] = idx;
+      posts[idx].comments = [];
+    });
+    comments.forEach((comment) => {
+      const { postId } = comment;
+      if (postIdToIdx[postId] !== undefined) {
+        posts[postIdToIdx[postId]].comments.push(comment);
       }
-    })
-  });
+    });
+    setPosts(posts);
+  }, []);
 
   const handleAddComment = (event) => {
-    if(event.key === 'Enter'){
+    if (event.key === "Enter") {
       console.log(event.target.value);
     }
-  }
+  };
 
   const handleAddPost = (event) => {
-    if(event.key === 'Enter'){
+    if (event.key === "Enter") {
       console.log(event.target.value);
     }
-  }
-
+  };
 
   const postsComponent = posts.map((post) => {
     const formattedDate = post.createdTime.toLocaleString("en-GB", {
@@ -37,37 +61,35 @@ const HomeComponent = () => {
       month: "short",
       year: "numeric",
       hour: "numeric",
-      minute: "2-digit"
+      minute: "2-digit",
     });
-    /*
+
     const commentsComponent = post.comments.map((comment) => {
       const formattedDate = comment.createdTime.toLocaleString("en-GB", {
         day: "numeric",
         month: "short",
         year: "numeric",
         hour: "numeric",
-        minute: "2-digit"
+        minute: "2-digit",
       });
       return (
         <div className="comment">
           <span className="commentUserName">
-            {comment.userName} 
+            {comment.userName}
             <span className="commentDate">{formattedDate}</span>
           </span>
           <span className="commentText">{comment.content}</span>
         </div>
       );
-    })
-    */
-    const commentsComponent = [];
+    });
 
-    commentsComponent.push((
-      <input 
+    commentsComponent.push(
+      <input
         className="addComment"
         placeholder="Add Comment..."
         onKeyPress={handleAddComment}
       />
-    ))
+    );
     return (
       <div className="post">
         <span className="postUserName">
@@ -78,7 +100,7 @@ const HomeComponent = () => {
         <div className="comments">{commentsComponent}</div>
       </div>
     );
-  })
+  });
 
   const displayName = sessionStorage.getItem("displayName");
 
@@ -87,27 +109,29 @@ const HomeComponent = () => {
     sessionStorage.removeItem("_id");
     sessionStorage.removeItem("displayName");
     sessionStorage.removeItem("access_token");
-    history.push('/login');
+    history.push("/login");
     history.go(0);
-  }
+  };
 
   return (
-  <div className="background">
-    <button className="logOut" onClick={handleLogOut}>Log Out</button>
-    <div className="mainBox">
-      <div className="posts">
-        <div className="newPost">
-          <span className="displayName">{displayName} : </span>
-          <input 
-            className="addPost"
-            placeholder="Add Post..."
-            onKeyPress={handleAddPost}
-          />
+    <div className="background">
+      <button className="logOut" onClick={handleLogOut}>
+        Log Out
+      </button>
+      <div className="mainBox">
+        <div className="posts">
+          <div className="newPost">
+            <span className="displayName">{displayName} : </span>
+            <input
+              className="addPost"
+              placeholder="Add Post..."
+              onKeyPress={handleAddPost}
+            />
+          </div>
+          {postsComponent}
         </div>
-        {postsComponent}
       </div>
     </div>
-  </div>
   );
 };
 
