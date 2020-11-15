c88acf822359925e0696d85a8f2e437eedb06c71
Tanakorn Pisnupoomi <intaniger@gmail.com>
2020-11-15 16:21:12 +0700

    fix: retreive postId from params instead of body

--- backend/src/guard/post.guard.ts
+++ backend/src/guard/post.guard.ts
@@ -1,7 +1,7 @@
 import {
-    CanActivate,
-    ExecutionContext,
-    Injectable,
+  CanActivate,
+  ExecutionContext,
+  Injectable
 } from '@nestjs/common';
 import { PostsService } from 'src/posts/posts.service';
 import { UsersService } from 'src/users/users.service';
@@ -13,13 +13,13 @@ export class PostGuard implements CanActivate {
     ) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        const request = context.switchToHttp().getRequest();
-        const reqUserId = request.user.userId;
-        const isAdmin = await this.userService.isAdmin(reqUserId);
-        if(isAdmin) return true;
-
-        const postId = request.body.postId;
-        const userId = await this.postsService.getPostUserIdByPostId(postId);
-        return userId === reqUserId;
+      const request = context.switchToHttp().getRequest();
+      const reqUserId = request.user.userId;
+      const isAdmin = await this.userService.isAdmin(reqUserId);
+      if(isAdmin) return true;
+      
+      const postId = request.params.postId;
+      const userId = await this.postsService.getPostUserIdByPostId(postId);
+      return userId === reqUserId;
     }
 }
